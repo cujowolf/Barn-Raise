72517de52a993e9e285552bfcb86b1e27baa26f6
publius <beanstalk.publius@protonmail.com>
2022-04-28 03:11:32 -0400

    Buy, Multi Token

--- protocol/contracts/BarnRaise.sol
+++ protocol/contracts/BarnRaise.sol
@@ -4,13 +4,21 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./ISwapRouter.sol";
+import "./IWETH.sol";
 
 /**
  * @author publius
  * @title Barn Raiser 
  */
 
-contract BarnRaise is Ownable {
+interface IERC20D {
+    function decimals() external view returns (uint8);
+}
+
+contract BarnRaise is Ownable, ReentrancyGuard {
 
     event CreateBarnRaise(
         uint256 bidStart, 
@@ -39,72 +47,149 @@ contract BarnRaise is Ownable {
         uint256 newBonus
     );
 
+    event Contribution(
+        address token,
+        uint256 amount
+    );
+
     using SafeERC20 for IERC20;
 
     /////////////////////// TESTING //////////////////////
-    // address constant public custodian = 0x925753106FCdB6D2f30C3db295328a0A1c5fD1D1; // Temporary: BF Multi-sig
-    // address constant public token = 0x389781BD602A7FFCBd1464c95C48f813FF24a8C6; // Temporary: Bean Token
-    // uint256 constant public bidStart = 1650891600;
+    address constant public custodian = 0x925753106FCdB6D2f30C3db295328a0A1c5fD1D1; // Temporary: BF Multi-sig
+    uint256 constant public bidStart = 1650891600;
 
     /////////////////////// TESTING //////////////////////
 
     // Bid Period Settings
-    uint256 constant public bidStart = 1651496400; // 5/2 9 AM PST
-    uint256 constant public bonusPerDay = 3;
-    uint256 constant public bidDays = 7;
+    // uint256 constant public bidStart      = 1651496400; // 5/2 9 AM PST
+    uint256 constant public bonusPerDay   = 3;
+    uint256 constant public bidDays       = 7;
     uint256 constant public secondsPerDay = 86400;
 
     // Barn Raise Settings
-    uint256 constant public start = bidStart + bidDays * secondsPerDay; // 5/9 9 AM PST
-    uint256 constant public length = 259200; // 3 days denominated in seconds. 3*24*60*60
+    uint256 constant public start       = bidStart + bidDays * secondsPerDay; // 5/9 9 AM PST
+    uint256 constant public length      = 259200; // 3 days denominated in seconds. 3*24*60*60
     uint256 constant public baseWeather = 20; // Start at 20% Weather
-    uint256 constant public step = 600; // 10 minutes denominated in seconds.
+    uint256 constant public step        = 600; // 10 minutes denominated in seconds.
+    uint256 constant public maxWeather  = 452;
 
-    // General Settings
-    address constant public custodian = 0x21DE18B6A8f78eDe6D16C50A167f6B222DC08DF7; // Temporary: BF Multi-sig
-    address constant public token = 0xDC59ac4FeFa32293A95889Dc396682858d52e5Db; // Temporary: Bean Token
-    uint256 constant target = 76_000_000 * 1e18;
-    uint256 constant maxWeather = 452;
+    // Raise Settings
+    // address constant public custodian = 0x21DE18B6A8f78eDe6D16C50A167f6B222DC08DF7; // Temporary: BF Multi-sig
+    uint256 constant target           = 77_000_000 * 1e18;
+    uint256 constant decimals         = 6;
+
+    // Uniswap Settings
+    address constant SWAP_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address constant WETH        = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address constant USDC        = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    uint24 constant POOL_FEE     = 500;
+
     uint256 public funded = 0; // A variable indicating if the Barn Raise as been fully funded.
+    mapping(address => uint256) whitelisted; // A mapping specifying whether a token is whitelisted or not.
 
     // Farmer => Weather => idx => amount We need to add idx in the case where a Farmer posts 2 Bids at the same Weather.
     mapping(bytes32 => uint256) bids;
 
-    constructor() {
+    constructor(address[] memory tokens) {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            whitelisted[tokens[i]] = 10 ** (IERC20D(tokens[i]).decimals() - decimals);
+        }
         _transferOwnership(_msgSender());
         emit CreateBarnRaise(bidStart, bonusPerDay, start, length, step, target);
     }
 
-    function sow(uint256 amount) external {
-        require(started() && !ended(), "Barn Raise: Not active.");
-        emit Sow(msg.sender, amount, getWeather());
-        IERC20(token).safeTransferFrom(msg.sender, custodian, amount);
+    ////////////////////////////////////////// SOW ////////////////////////////////////////////////
+
+    function buyAndSow(uint256 minBuyAmount, uint256 amount) external payable nonReentrant {
+        uint256 amountOut = buy(minBuyAmount);
+        _sow(amount + amountOut);
+        sendToken(USDC, amount);
+        emit Contribution(USDC, amount + amountOut);
     }
 
-    function createBid(uint256 amount, uint256 weather) external {
+    function sow(address token, uint256 amount) external nonReentrant {
+        _sow(amount / whitelisted[token]);
+        sendToken(token, amount);
+        emit Contribution(token, amount);
+    }
+
+    function _sow(uint256 amount) private {
+        require(started() && !ended(), "Barn Raise: Not active.");
+        emit Sow(msg.sender, amount, getWeather());
+    }
+
+    ///////////////////////////////////// CREATE BID //////////////////////////////////////////////
+
+    function buyAndCreateBid(uint256 minBuyAmount, uint256 amount, uint256 weather) external payable nonReentrant {
+        uint256 amountOut = buy(minBuyAmount);
+        sendToken(USDC, amount);
+        _createBid(amount + minBuyAmount, weather);
+        emit Contribution(USDC, amount + amountOut);
+    }
+
+    function createBid(address token, uint256 amount, uint256 weather) external nonReentrant {
+        _createBid(amount / whitelisted[token], weather);
+        sendToken(token, amount);
+        emit Contribution(token, amount);
+    }
+
+    function _createBid(uint256 amount, uint256 weather) private {
         uint256 bonus;
         (weather, bonus) = checkBid(weather);
         uint256 idx = saveBid(amount, weather);
         emit CreateBid(msg.sender, amount, weather, idx, bonus);
-        IERC20(token).safeTransferFrom(msg.sender, custodian, amount);
     }
 
-    function updateBid(uint256 newAmount, uint256 prevWeather, uint256 prevIdx, uint256 newWeather) external {
+    ///////////////////////////////////// UPDATE BID //////////////////////////////////////////////
+
+    function buyAndUpdateBid(
+        uint256 minBuyAmount, 
+        uint256 newAmount, 
+        uint256 prevWeather, 
+        uint256 prevIdx, 
+        uint256 newWeather
+    ) external payable nonReentrant {
+        uint256 amountOut = buy(minBuyAmount);
+        newAmount = _updateBid(newAmount, amountOut, prevWeather, prevIdx, newWeather);
+        emit Contribution(USDC, newAmount + amountOut);
+        if (newAmount > 0) sendToken(USDC, newAmount);
+    }
+
+    function updateBid(
+        address token, 
+        uint256 newAmount, 
+        uint256 prevWeather, 
+        uint256 prevIdx, 
+        uint256 newWeather
+    ) external nonReentrant {
+        newAmount = _updateBid(newAmount, 0, prevWeather, prevIdx, newWeather);
+        if (newAmount > 0) {
+            emit Contribution(token, newAmount);
+            sendToken(token, newAmount * whitelisted[token]);
+        }
+    }
+
+    function _updateBid(
+        uint256 newAmount,
+        uint256 extraAmount,
+        uint256 prevWeather, 
+        uint256 prevIdx, 
+        uint256 newWeather
+    ) private returns (uint256 transferAmount) {
         uint256 bonus;
         (newWeather, bonus) = checkBid(newWeather);
         require(newWeather < prevWeather, "Barn Raise: Weather not valid.");
         uint256 prevAmount = deleteBid(newAmount, prevWeather, prevIdx);
-        uint256 newIdx = saveBid(newAmount, newWeather);
-        newAmount -= prevAmount;
-        emit UpdateBid(msg.sender, prevAmount, newAmount, prevWeather, prevIdx, newWeather, newIdx, bonus);
-        if (newAmount > 0) IERC20(token).safeTransferFrom(msg.sender, custodian, newAmount);
+        uint256 newIdx = saveBid(newAmount + extraAmount, newWeather);
+        transferAmount = newAmount - prevAmount;
+        emit UpdateBid(msg.sender, prevAmount, transferAmount + extraAmount, prevWeather, prevIdx, newWeather, newIdx, bonus);
     }
+
+    ///////////////////////////////////// Barn Raise //////////////////////////////////////////////
 
     function setFunded(uint256 f) external onlyOwner {
         funded = f;
     }
-
-    // Barn Raise
 
     function started() public view returns (bool) {
         return block.timestamp >= start;
@@ -119,14 +204,10 @@ contract BarnRaise is Ownable {
         w = (block.timestamp - start) / step + baseWeather;
     }
 
-    // Bid Period
+    ///////////////////////////////////// Bid Period //////////////////////////////////////////////
 
     function bid(bytes32 idx) external view returns (uint256) {
         return bids[idx];
-    }
-
-    function biddingStarted() public view returns (bool) {
-        return block.timestamp >= bidStart;
     }
 
     function getBonus() public view returns (uint256 b) {
@@ -134,23 +215,20 @@ contract BarnRaise is Ownable {
         b = ((start - block.timestamp - 1) / secondsPerDay + 1) * bonusPerDay;
     }
 
-    // Helpers
- 
-    function getMaxWeather(uint256 wea) private view returns (uint256 w) {
-        w = getWeather();
-        if (wea > w) w = wea;
+    function biddingStarted() public view returns (bool) {
+        return block.timestamp >= bidStart;
     }
 
     function checkBid(uint256 weather) private view returns (uint256 w, uint256 b) {
         require(biddingStarted() && !ended(), "Barn Raise: Bidding not active.");
         require(weather <= maxWeather, "Barn Raise: Weather too high.");
-        w = getMaxWeather(weather);
+         w = getWeather();
+        if (weather > w) w = weather;
         b = getBonus();
     }
 
     function saveBid(uint256 amount, uint256 weather) private returns (uint256 idx) {
         idx = block.timestamp;
-        // CHECK IF HASH HERE IS CHEAPER!
         bytes32 hashId = createBidId(msg.sender, weather, idx);
         while (bids[hashId] > 0) {
             ++idx;
@@ -169,5 +247,32 @@ contract BarnRaise is Ownable {
 
     function createBidId(address account, uint256 w, uint256 idx) private pure returns (bytes32 id) {
         id = keccak256(abi.encodePacked(account, w, idx));
+    }
+
+    ///////////////////////////////////// Contributing //////////////////////////////////////////////
+
+    function isWhitelisted(address token) public view returns (bool) {
+        return whitelisted[token] > 0;
+    }
+
+    function sendToken(address token, uint256 amount) private {
+        require(isWhitelisted(token), "Barn Raise: not whitelisted.");
+        IERC20(token).safeTransferFrom(msg.sender, custodian, amount);
+    }
+
+    function buy(uint256 minAmountOut) private returns (uint256 amountOut) {
+        IWETH(WETH).deposit{value: msg.value}();
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: WETH,
+                tokenOut: USDC,
+                fee: POOL_FEE,
+                recipient: custodian,
+                deadline: block.timestamp,
+                amountIn: msg.value,
+                amountOutMinimum: minAmountOut,
+                sqrtPriceLimitX96: 0
+            });
+        amountOut = ISwapRouter(SWAP_ROUTER).exactInputSingle(params);
     }
 }
--- protocol/test/BarnRaise.test.js
+++ protocol/test/BarnRaise.test.js
@@ -1,5 +1,4 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 const fs = require('fs');
 
 const BEAN = '0xDC59ac4FeFa32293A95889Dc396682858d52e5Db';
@@ -15,6 +14,10 @@ function to18(amount) {
   return ethers.utils.parseEther(amount);
 }
 
+function to6(amount) {
+  return ethers.utils.parseUnits(amount, 6);
+}
+
 describe("Barn Raise", function () {
 
   beforeEach(async function () {
@@ -22,18 +25,19 @@ describe("Barn Raise", function () {
 
     [owner,user] = await ethers.getSigners();
 
-    const BarnRaise = await ethers.getContractFactory('BarnRaise')
-    this.barnRaise = await BarnRaise.connect(owner).deploy();
-    await this.barnRaise.deployed()
-
     let MockToken = fs.readFileSync(`./artifacts/contracts/MockToken.sol/MockToken.json`);
     await network.provider.send("hardhat_setCode", [BEAN, JSON.parse(MockToken).deployedBytecode]);
     this.token = await ethers.getContractAt('MockToken', BEAN);
-    
+
+    await this.token.setDecimals(18)
+
+    const BarnRaise = await ethers.getContractFactory('BarnRaise')
+    this.barnRaise = await BarnRaise.connect(owner).deploy([this.token.address]);
+    await this.barnRaise.deployed()
+
     await owner.sendTransaction({ to: BF, value: ethers.utils.parseEther("2") });
     await hre.network.provider.request({ method: "hardhat_impersonateAccount", params: [BF] });
     const bf = await ethers.getSigner(BF)
-
 
     await this.token.connect(bf).burn(await this.token.balanceOf(BF))
     await this.token.mint(user.address, ethers.utils.parseEther('1000000'))
@@ -47,34 +51,34 @@ describe("Barn Raise", function () {
 
   it('Sow', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [start+1])
-    const result = await this.barnRaise.connect(user).sow(to18('1'));
+    const result = await this.barnRaise.connect(user).sow(this.token.address, to18('1'));
     expect(await this.token.balanceOf(BF)).to.equal(to18('1'));
-    await expect(result).to.emit(this.barnRaise, 'Sow').withArgs(user.address, to18('1'), '20');
+    await expect(result).to.emit(this.barnRaise, 'Sow').withArgs(user.address, to6('1'), '20');
   })
 
   it('Create Bid with bonus', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [bidStart+1])
-    const result = await this.barnRaise.connect(user).createBid(to18('1'), '21');
+    const result = await this.barnRaise.connect(user).createBid(this.token.address, to18('1'), '21');
     expect(await this.token.balanceOf(BF)).to.equal(to18('1'));
-    await expect(result).to.emit(this.barnRaise, 'CreateBid').withArgs(user.address, to18('1'), '21', bidStart+1, '21');
+    await expect(result).to.emit(this.barnRaise, 'CreateBid').withArgs(user.address, to6('1'), '21', bidStart+1, '21');
   })
 
   it('Create Bid below min weather', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [bidStart+timePerDay])
-    const result = await this.barnRaise.connect(user).createBid(to18('1'), '1');
+    const result = await this.barnRaise.connect(user).createBid(this.token.address, to18('1'), '1');
     expect(await this.token.balanceOf(BF)).to.equal(to18('1'));
-    await expect(result).to.emit(this.barnRaise, 'CreateBid').withArgs(user.address, to18('1'), '1', bidStart+timePerDay, '18');
+    await expect(result).to.emit(this.barnRaise, 'CreateBid').withArgs(user.address, to6('1'), '1', bidStart+timePerDay, '18');
   })
 
   it('Update Bid', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [start - 2])
-    await this.barnRaise.connect(user).createBid(to18('1'), '2');
-    const result = await this.barnRaise.connect(user).updateBid(to18('1'), '2', start - 2, '1');
+    await this.barnRaise.connect(user).createBid(this.token.address, to18('1'), '2');
+    const result = await this.barnRaise.connect(user).updateBid(this.token.address, to6('1'), '2', start - 2, '1');
     expect(await this.token.balanceOf(BF)).to.equal(to18('1'));
     await expect(result).to.emit(this.barnRaise, 'UpdateBid').withArgs(
       user.address, 
-      to18('1'),
-      to18('0'),
+      to6('1'),
+      to6('0'),
       '2', 
       start - 2,
       '1', 
@@ -85,13 +89,13 @@ describe("Barn Raise", function () {
 
   it('Create and Update Bid', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [start + 601])
-    await this.barnRaise.connect(user).createBid(to18('1'), '25');
-    const result = await this.barnRaise.connect(user).updateBid(to18('2'), '25', start + 601, '3');
+    await this.barnRaise.connect(user).createBid(this.token.address, to18('1'), '25');
+    const result = await this.barnRaise.connect(user).updateBid(this.token.address, to6('2'), '25', start + 601, '3');
     expect(await this.token.balanceOf(BF)).to.equal(to18('2'));
     await expect(result).to.emit(this.barnRaise, 'UpdateBid').withArgs(
       user.address, 
-      to18('1'),
-      to18('1'),
+      to6('1'),
+      to6('1'),
       '25', 
       start + 601,
       '21', 
@@ -102,13 +106,13 @@ describe("Barn Raise", function () {
 
   it('Create and Update Bid', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [start + 601])
-    await this.barnRaise.connect(user).createBid(to18('1'), '25');
-    const result = await this.barnRaise.connect(user).updateBid(to18('0.5'), '25', start + 601, '3');
+    await this.barnRaise.connect(user).createBid(this.token.address, to18('1'), '25');
+    const result = await this.barnRaise.connect(user).updateBid(this.token.address, to6('0.5'), '25', start + 601, '3');
     expect(await this.token.balanceOf(BF)).to.equal(to18('1'));
     await expect(result).to.emit(this.barnRaise, 'UpdateBid').withArgs(
       user.address, 
-      to18('0.5'), 
-      to18('0'),
+      to6('0.5'), 
+      to6('0'),
       '25', 
       start + 601,
       '21', 
@@ -119,13 +123,13 @@ describe("Barn Raise", function () {
 
   it('reverts if update Bid with lower weather', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [bidStart+timePerDay])
-    await this.barnRaise.connect(user).createBid(to18('1'), '20')
-    await expect(this.barnRaise.connect(user).updateBid(to18('1'), '20', bidStart+timePerDay, '25')).to.be.revertedWith('Barn Raise: Weather not valid.');
+    await this.barnRaise.connect(user).createBid(this.token.address, to18('1'), '20')
+    await expect(this.barnRaise.connect(user).updateBid(this.token.address, to18('1'), '20', bidStart+timePerDay, '25')).to.be.revertedWith('Barn Raise: Weather not valid.');
   })
 
   it('reverts if update Bid for non-existent Bid', async function () {
     await network.provider.send("evm_setNextBlockTimestamp", [bidStart+timePerDay])
-    await expect(this.barnRaise.connect(user).updateBid(to18('1'), '30', bidStart+timePerDay, '25')).to.be.revertedWith('Barn Raise: Bid not valid.');
+    await expect(this.barnRaise.connect(user).updateBid(this.token.address, to18('1'), '30', bidStart+timePerDay, '25')).to.be.revertedWith('Barn Raise: Bid not valid.');
   })
 
   describe("End Barn Raise", async function () {
@@ -136,40 +140,40 @@ describe("Barn Raise", function () {
     it('ends Barn Raise', async function () {
       await network.provider.send("evm_setNextBlockTimestamp", [start + 601])
       await this.barnRaise.connect(owner).setFunded('1')
-      await expect(this.barnRaise.sow(to18('1'))).to.be.revertedWith('Barn Raise: Not active.')
-      await expect(this.barnRaise.createBid(to18('1'), '1')).to.be.revertedWith('Barn Raise: Bidding not active.')
-      await expect(this.barnRaise.updateBid(to18('1'), '1', '123', '1')).to.be.revertedWith('Barn Raise: Bidding not active.')
+      await expect(this.barnRaise.sow(this.token.address, to18('1'))).to.be.revertedWith('Barn Raise: Not active.')
+      await expect(this.barnRaise.createBid(this.token.address, to18('1'), '1')).to.be.revertedWith('Barn Raise: Bidding not active.')
+      await expect(this.barnRaise.updateBid(this.token.address, to18('1'), '1', '123', '1')).to.be.revertedWith('Barn Raise: Bidding not active.')
     })
   })
 
   describe("Sow", async function () {
     it("Reverts if early", async function () {
       await network.provider.send("evm_setNextBlockTimestamp", [parseInt(start) - 1])
-      await expect(this.barnRaise.sow(to18('1'))).to.be.revertedWith('Barn Raise: Not active.')
+      await expect(this.barnRaise.sow(this.token.address, to18('1'))).to.be.revertedWith('Barn Raise: Not active.')
     })
 
     it("Reverts if late", async function () {
       await network.provider.send("evm_setNextBlockTimestamp", [parseInt(end) + 1])
-      await expect(this.barnRaise.sow(to18('1'))).to.be.revertedWith('Barn Raise: Not active.')
+      await expect(this.barnRaise.sow(this.token.address, to18('1'))).to.be.revertedWith('Barn Raise: Not active.')
     })
   });
 
   describe("Bid", async function () {
     it("Reverts if early", async function () {
       await network.provider.send("evm_setNextBlockTimestamp", [parseInt(bidStart) - 1])
-      await expect(this.barnRaise.createBid(to18('1'), 100)).to.be.revertedWith('Barn Raise: Bidding not active.')
+      await expect(this.barnRaise.createBid(this.token.address, to18('1'), 100)).to.be.revertedWith('Barn Raise: Bidding not active.')
     })
     it("Reverts if early", async function () {
       await network.provider.send("evm_setNextBlockTimestamp", [parseInt(bidStart) - 1])
-      await expect(this.barnRaise.updateBid(to18('1'), 100, '123', 110)).to.be.revertedWith('Barn Raise: Bidding not active.')
+      await expect(this.barnRaise.updateBid(this.token.address, to18('1'), 100, '123', 110)).to.be.revertedWith('Barn Raise: Bidding not active.')
     })
     it("Reverts if late", async function () {
       await network.provider.send("evm_setNextBlockTimestamp", [parseInt(end) + 1])
-      await expect(this.barnRaise.createBid(to18('1'), 100)).to.be.revertedWith('Barn Raise: Bidding not active.')
+      await expect(this.barnRaise.createBid(this.token.address, to18('1'), 100)).to.be.revertedWith('Barn Raise: Bidding not active.')
     })
     it("Reverts if late", async function () {
       await network.provider.send("evm_setNextBlockTimestamp", [parseInt(end) + 1])
-      await expect(this.barnRaise.updateBid(to18('1'), 100, '123', 110)).to.be.revertedWith('Barn Raise: Bidding not active.')
+      await expect(this.barnRaise.updateBid(this.token.address, to18('1'), 100, '123', 110)).to.be.revertedWith('Barn Raise: Bidding not active.')
     })
   });
 });
